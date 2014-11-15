240612d28f603e06ad75ff7aaa07a538fa5a6dcb
peterbuk8 <peterbuk8@gmail.com>
2014-11-15 12:21:03 -0700

    MP3 tagging enabled
    Basic mp3 tag options are added

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -34,8 +34,19 @@
             this.txtBoxURL = new System.Windows.Forms.TextBox();
             this.txtBoxFileName = new System.Windows.Forms.TextBox();
             this.btnDownload = new System.Windows.Forms.Button();
-            this.lblProgress = new System.Windows.Forms.Label();
             this.lblLinkAbout = new System.Windows.Forms.LinkLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.boxFields = new System.Windows.Forms.GroupBox();
+            this.txtBoxAlbum = new System.Windows.Forms.TextBox();
+            this.txtBoxArtist = new System.Windows.Forms.TextBox();
+            this.txtBoxTitle = new System.Windows.Forms.TextBox();
+            this.lblPromptAlbum = new System.Windows.Forms.Label();
+            this.lblPromptArtist = new System.Windows.Forms.Label();
+            this.lblPromptTitle = new System.Windows.Forms.Label();
+            this.chkBoxDefault = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.boxFields.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblPromptURL
@@ -50,7 +61,7 @@
             // lblPromptFileName
             // 
             this.lblPromptFileName.AutoSize = true;
-            this.lblPromptFileName.Location = new System.Drawing.Point(27, 84);
+            this.lblPromptFileName.Location = new System.Drawing.Point(15, 27);
             this.lblPromptFileName.Name = "lblPromptFileName";
             this.lblPromptFileName.Size = new System.Drawing.Size(82, 13);
             this.lblPromptFileName.TabIndex = 1;
@@ -62,17 +73,20 @@
             this.txtBoxURL.Name = "txtBoxURL";
             this.txtBoxURL.Size = new System.Drawing.Size(207, 20);
             this.txtBoxURL.TabIndex = 2;
+            this.txtBoxURL.Enter += new System.EventHandler(this.txtBoxRecolor);
             // 
             // txtBoxFileName
             // 
-            this.txtBoxFileName.Location = new System.Drawing.Point(30, 100);
+            this.txtBoxFileName.Enabled = false;
+            this.txtBoxFileName.Location = new System.Drawing.Point(18, 43);
             this.txtBoxFileName.Name = "txtBoxFileName";
-            this.txtBoxFileName.Size = new System.Drawing.Size(207, 20);
+            this.txtBoxFileName.Size = new System.Drawing.Size(174, 20);
             this.txtBoxFileName.TabIndex = 3;
+            this.txtBoxFileName.Text = "(default from video)";
             // 
             // btnDownload
             // 
-            this.btnDownload.Location = new System.Drawing.Point(262, 58);
+            this.btnDownload.Location = new System.Drawing.Point(273, 32);
             this.btnDownload.Name = "btnDownload";
             this.btnDownload.Size = new System.Drawing.Size(89, 39);
             this.btnDownload.TabIndex = 4;
@@ -80,42 +94,139 @@
             this.btnDownload.UseVisualStyleBackColor = true;
             this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
             // 
-            // lblProgress
-            // 
-            this.lblProgress.Location = new System.Drawing.Point(27, 142);
-            this.lblProgress.Name = "lblProgress";
-            this.lblProgress.Size = new System.Drawing.Size(324, 23);
-            this.lblProgress.TabIndex = 5;
-            this.lblProgress.Text = "Welcome!";
-            // 
             // lblLinkAbout
             // 
             this.lblLinkAbout.AutoSize = true;
-            this.lblLinkAbout.Location = new System.Drawing.Point(337, 157);
+            this.lblLinkAbout.Location = new System.Drawing.Point(335, 299);
             this.lblLinkAbout.Name = "lblLinkAbout";
             this.lblLinkAbout.Size = new System.Drawing.Size(35, 13);
             this.lblLinkAbout.TabIndex = 6;
             this.lblLinkAbout.TabStop = true;
             this.lblLinkAbout.Text = "About";
-            this.lblLinkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.lblLinkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblLinkAbout_LinkClicked);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 312);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(374, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(60, 17);
+            this.statusLabel.Text = "Welcome!";
+            // 
+            // boxFields
+            // 
+            this.boxFields.Controls.Add(this.chkBoxDefault);
+            this.boxFields.Controls.Add(this.txtBoxAlbum);
+            this.boxFields.Controls.Add(this.txtBoxArtist);
+            this.boxFields.Controls.Add(this.txtBoxTitle);
+            this.boxFields.Controls.Add(this.lblPromptAlbum);
+            this.boxFields.Controls.Add(this.lblPromptArtist);
+            this.boxFields.Controls.Add(this.lblPromptTitle);
+            this.boxFields.Controls.Add(this.txtBoxFileName);
+            this.boxFields.Controls.Add(this.lblPromptFileName);
+            this.boxFields.Location = new System.Drawing.Point(30, 87);
+            this.boxFields.Name = "boxFields";
+            this.boxFields.Size = new System.Drawing.Size(232, 208);
+            this.boxFields.TabIndex = 8;
+            this.boxFields.TabStop = false;
+            this.boxFields.Text = "MP3 Tags";
+            // 
+            // txtBoxAlbum
+            // 
+            this.txtBoxAlbum.Enabled = false;
+            this.txtBoxAlbum.Location = new System.Drawing.Point(18, 185);
+            this.txtBoxAlbum.Name = "txtBoxAlbum";
+            this.txtBoxAlbum.Size = new System.Drawing.Size(174, 20);
+            this.txtBoxAlbum.TabIndex = 9;
+            this.txtBoxAlbum.Text = "Nico Nico Douga Music";
+            // 
+            // txtBoxArtist
+            // 
+            this.txtBoxArtist.Enabled = false;
+            this.txtBoxArtist.Location = new System.Drawing.Point(18, 139);
+            this.txtBoxArtist.Name = "txtBoxArtist";
+            this.txtBoxArtist.Size = new System.Drawing.Size(174, 20);
+            this.txtBoxArtist.TabIndex = 8;
+            this.txtBoxArtist.Text = "(default from video)";
+            // 
+            // txtBoxTitle
+            // 
+            this.txtBoxTitle.Enabled = false;
+            this.txtBoxTitle.Location = new System.Drawing.Point(18, 91);
+            this.txtBoxTitle.Name = "txtBoxTitle";
+            this.txtBoxTitle.Size = new System.Drawing.Size(174, 20);
+            this.txtBoxTitle.TabIndex = 7;
+            this.txtBoxTitle.Text = "(default from video)";
+            // 
+            // lblPromptAlbum
+            // 
+            this.lblPromptAlbum.AutoSize = true;
+            this.lblPromptAlbum.Location = new System.Drawing.Point(15, 171);
+            this.lblPromptAlbum.Name = "lblPromptAlbum";
+            this.lblPromptAlbum.Size = new System.Drawing.Size(39, 13);
+            this.lblPromptAlbum.TabIndex = 6;
+            this.lblPromptAlbum.Text = "Album:";
+            // 
+            // lblPromptArtist
+            // 
+            this.lblPromptArtist.AutoSize = true;
+            this.lblPromptArtist.Location = new System.Drawing.Point(15, 123);
+            this.lblPromptArtist.Name = "lblPromptArtist";
+            this.lblPromptArtist.Size = new System.Drawing.Size(33, 13);
+            this.lblPromptArtist.TabIndex = 5;
+            this.lblPromptArtist.Text = "Artist:";
+            // 
+            // lblPromptTitle
+            // 
+            this.lblPromptTitle.AutoSize = true;
+            this.lblPromptTitle.Location = new System.Drawing.Point(15, 75);
+            this.lblPromptTitle.Name = "lblPromptTitle";
+            this.lblPromptTitle.Size = new System.Drawing.Size(30, 13);
+            this.lblPromptTitle.TabIndex = 4;
+            this.lblPromptTitle.Text = "Title:";
+            // 
+            // chkBoxDefault
+            // 
+            this.chkBoxDefault.AutoSize = true;
+            this.chkBoxDefault.Checked = true;
+            this.chkBoxDefault.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBoxDefault.Location = new System.Drawing.Point(144, 0);
+            this.chkBoxDefault.Name = "chkBoxDefault";
+            this.chkBoxDefault.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chkBoxDefault.Size = new System.Drawing.Size(87, 17);
+            this.chkBoxDefault.TabIndex = 11;
+            this.chkBoxDefault.Text = "Use Defaults";
+            this.chkBoxDefault.UseVisualStyleBackColor = true;
+            this.chkBoxDefault.CheckStateChanged += new System.EventHandler(this.chkBoxDefault_CheckStateChanged);
             // 
             // NSRForm
             // 
             this.AcceptButton = this.btnDownload;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 176);
+            this.ClientSize = new System.Drawing.Size(374, 334);
+            this.Controls.Add(this.boxFields);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.lblLinkAbout);
-            this.Controls.Add(this.lblProgress);
             this.Controls.Add(this.btnDownload);
-            this.Controls.Add(this.txtBoxFileName);
             this.Controls.Add(this.txtBoxURL);
-            this.Controls.Add(this.lblPromptFileName);
             this.Controls.Add(this.lblPromptURL);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "NSRForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nico Sound Reborn";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.boxFields.ResumeLayout(false);
+            this.boxFields.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,8 +239,17 @@
         private System.Windows.Forms.TextBox txtBoxURL;
         private System.Windows.Forms.TextBox txtBoxFileName;
         private System.Windows.Forms.Button btnDownload;
-        private System.Windows.Forms.Label lblProgress;
         private System.Windows.Forms.LinkLabel lblLinkAbout;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.GroupBox boxFields;
+        private System.Windows.Forms.TextBox txtBoxAlbum;
+        private System.Windows.Forms.TextBox txtBoxArtist;
+        private System.Windows.Forms.TextBox txtBoxTitle;
+        private System.Windows.Forms.Label lblPromptAlbum;
+        private System.Windows.Forms.Label lblPromptArtist;
+        private System.Windows.Forms.Label lblPromptTitle;
+        private System.Windows.Forms.CheckBox chkBoxDefault;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -6,8 +6,10 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 /*
  * 
@@ -26,20 +28,102 @@ namespace NicoSoundReborn
 {
     public partial class NSRForm : Form
     {
+        private bool tagExtracted;
+
         public NSRForm()
         {
             InitializeComponent();
+
+            tagExtracted = false;
         }
 
+
+        /***************************************************************************************************************************/
+
+        /*
+         * Download button clicked. Start download process.
+         */
         private void btnDownload_Click(object sender, EventArgs e)
         {
-            // check if url is provided
-            if (txtBoxURL.Text.Equals(""))
-                lblProgress.Text = "Enter an appropriate NND URL";
+            // check if proper url is provided
+            if (!CheckURL(txtBoxURL.Text))
+            {
+                statusLabel.Text = "Invalid URL. Enter an appropriate NND URL.";
+                txtBoxURL.BackColor = Color.LightPink;
+            }
             else
-                // TO DO: what if the URL is invalid?
                 DownloadCommands();
+        }
 
+        /*
+         * Change color of textbox back to white
+         */
+        private void txtBoxRecolor(object sender, EventArgs e)
+        {
+            TextBox txtBox = (TextBox)sender;
+            txtBox.BackColor = Color.White;
+        }
+
+        /*
+         * About link clicked. Show messagebox info.
+         */
+        private void lblLinkAbout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string about = "Version 1.2\n" +
+                            "Author: Peter Buk\n\n" +
+                            "This program makes use of these great utilities:\n" +
+                            "https://www.ffmpeg.org/\n" +
+                            "http://rg3.github.io/youtube-dl/index.html\n";
+
+            MessageBox.Show(about, "About");
+        }
+
+        /*
+         *  Changes default values of MP3 tags
+         */
+        private void chkBoxDefault_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (chkBoxDefault.Checked == true)
+            {
+                txtBoxFileName.Enabled = false;
+                txtBoxTitle.Enabled = false;
+                txtBoxArtist.Enabled = false;
+                txtBoxAlbum.Enabled = false;
+
+                txtBoxFileName.Text = "(default from video)";
+                txtBoxTitle.Text = "(default from video)";
+                txtBoxArtist.Text = "(default from video)";
+                txtBoxAlbum.Text = "Nico Nico Douga Music";
+            }
+            else if (chkBoxDefault.Checked == false)
+            {
+                txtBoxFileName.Enabled = true;
+                txtBoxTitle.Enabled = true;
+                txtBoxArtist.Enabled = true;
+                txtBoxAlbum.Enabled = true;
+
+                txtBoxFileName.Text = "";
+                txtBoxTitle.Text = "";
+                txtBoxArtist.Text = "";
+                txtBoxAlbum.Text = "";
+            }
+        }
+
+        /***************************************************************************************************************************/
+
+        /*
+         *  Checks for a valid NND url
+         */
+        private bool CheckURL(string URL)
+        {
+            Regex r = new Regex("https?://(?:www\\.|secure\\.)?nicovideo\\.jp/watch/((?:[a-z]{2})?[0-9]+)");
+
+            Match m = r.Match(URL);
+
+            if (m.Success)
+                return true;
+            else
+                return false;
         }
 
         /*
@@ -54,13 +138,16 @@ namespace NicoSoundReborn
             // set process params to download
             startInfo.Arguments = "/C youtube-dl " + txtBoxURL.Text;
             startInfo.FileName = "cmd.exe";
-            //startInfo.UseShellExecute = false;
-            //startInfo.RedirectStandardOutput = true;
-            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-
+            
+            /* Gotta figure out how to redirect output
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            */
+            
             p.StartInfo = startInfo;
             p.Start();
-            lblProgress.Text = "Downloading...";
+            statusLabel.Text = "Downloading...";
 
             /*while (p.HasExited == false)
             {
@@ -70,30 +157,48 @@ namespace NicoSoundReborn
 
             p.WaitForExit();
 
-            lblProgress.Text = "Converting...";
+            statusLabel.Text = "Converting...";
 
-            // TO DO: extract mp4 name
+            // extract mp4 name
+            string filename = ExtractFileName();
+
+            // set up mp3 tags
+            if (chkBoxDefault.Checked == true && filename != null)  // use default
+            {
+                txtBoxFileName.Text = filename + ".mp3";
+                txtBoxTitle.Text = filename;
+                txtBoxArtist.Text = filename;  //TODO: attempt to extract artist name...
+
+                tagExtracted = true;
+            }
+            else if (chkBoxDefault.Checked == true && filename == null) // extracting failed, use placeholders
+            {
+                txtBoxFileName.Text = "song.mp3";
+                txtBoxTitle.Text = "";
+                txtBoxArtist.Text = "";
+
+                tagExtracted = false;
+            }
+            else  // make sure the user inputted file name has *.mp3
+            {
+                if (txtBoxFileName.Text.Equals("")) // empty filename
+                    txtBoxFileName.Text = "song.mp3";
+                else
+                    if (!txtBoxFileName.Text.EndsWith(".mp3", true, null)) // did not have .mp3
+                        txtBoxFileName.Text = txtBoxFileName.Text + ".mp3";
+            }
 
             // rename mp4
             startInfo.Arguments = "/C ren *.mp4 tmp.mp4";
             p.Start();
             p.WaitForExit();
 
-            // check if user inputted file name, otherwise use default
-            // also check for ".mp3" and add automatically if not present
-            string fileName;
-            if (txtBoxFileName.Text.Equals(""))
-                fileName = "song.mp3";
-            else
-            {
-                if (txtBoxFileName.Text.EndsWith(".mp3", true, null))
-                    fileName = "\"" + txtBoxFileName.Text + "\"";
-                else
-                    fileName = "\"" + txtBoxFileName.Text + ".mp3" +"\"";
-            }
-
             // convert mp4
-            startInfo.Arguments = "/C ffmpeg -i tmp.mp4 -f mp3 -ab 320000 -vn " + fileName;
+            startInfo.Arguments = "/C ffmpeg -i tmp.mp4 -f mp3 -ab 320000 -vn " +
+                                    "-metadata title=\"" + txtBoxTitle.Text +
+                                    "\" -metadata artist=\"" + txtBoxArtist.Text +
+                                    "\" -metadata album=\"" + txtBoxAlbum.Text +
+                                    "\" " + txtBoxFileName.Text;
             p.Start();
             p.WaitForExit();
 
@@ -102,19 +207,24 @@ namespace NicoSoundReborn
             p.Start();
             p.WaitForExit();
 
-            lblProgress.Text = "Complete!";
+            statusLabel.Text = "Complete!";
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        /*
+         *  Extract the filename of the downloaded mp4
+         *  Known issues: will grab the first mp4 found, can't check for latest yet
+         */
+        private string ExtractFileName()
         {
-            string about = "Version 1.1\n" +
-                            "Author: Peter Buk\n\n" +
-                            "This program makes use of these great utilities:\n" +
-                            "https://www.ffmpeg.org/\n" +
-                            "http://rg3.github.io/youtube-dl/index.html\n";
+            string pwd = Path.GetDirectoryName(Application.ExecutablePath);
+            string[] filePath = Directory.GetFiles(pwd, "*.mp4");
 
-            MessageBox.Show(about, "About");
+            if (filePath.Length != 0)
+                return Path.GetFileNameWithoutExtension(filePath[0]);
+            else
+                return null;
         }
+
 
         
 
