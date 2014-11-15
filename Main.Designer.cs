f852d14cd8309f743c78f5ec3802448278da7ede
peterbuk8 <peterbuk8@gmail.com>
2014-11-15 09:55:49 -0700

    Initial commit
    We're going open source!

---
+++ Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace NicoSoundReborn
+{
+    partial class NSRForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NSRForm));
+            this.lblPromptURL = new System.Windows.Forms.Label();
+            this.lblPromptFileName = new System.Windows.Forms.Label();
+            this.txtBoxURL = new System.Windows.Forms.TextBox();
+            this.txtBoxFileName = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.lblLinkAbout = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lblPromptURL
+            // 
+            this.lblPromptURL.AutoSize = true;
+            this.lblPromptURL.Location = new System.Drawing.Point(27, 26);
+            this.lblPromptURL.Name = "lblPromptURL";
+            this.lblPromptURL.Size = new System.Drawing.Size(59, 13);
+            this.lblPromptURL.TabIndex = 0;
+            this.lblPromptURL.Text = "NND URL:";
+            // 
+            // lblPromptFileName
+            // 
+            this.lblPromptFileName.AutoSize = true;
+            this.lblPromptFileName.Location = new System.Drawing.Point(27, 84);
+            this.lblPromptFileName.Name = "lblPromptFileName";
+            this.lblPromptFileName.Size = new System.Drawing.Size(82, 13);
+            this.lblPromptFileName.TabIndex = 1;
+            this.lblPromptFileName.Text = "MP3 File Name:";
+            // 
+            // txtBoxURL
+            // 
+            this.txtBoxURL.Location = new System.Drawing.Point(30, 42);
+            this.txtBoxURL.Name = "txtBoxURL";
+            this.txtBoxURL.Size = new System.Drawing.Size(207, 20);
+            this.txtBoxURL.TabIndex = 2;
+            // 
+            // txtBoxFileName
+            // 
+            this.txtBoxFileName.Location = new System.Drawing.Point(30, 100);
+            this.txtBoxFileName.Name = "txtBoxFileName";
+            this.txtBoxFileName.Size = new System.Drawing.Size(207, 20);
+            this.txtBoxFileName.TabIndex = 3;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(262, 58);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(89, 39);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Location = new System.Drawing.Point(27, 142);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(324, 23);
+            this.lblProgress.TabIndex = 5;
+            this.lblProgress.Text = "Welcome!";
+            // 
+            // lblLinkAbout
+            // 
+            this.lblLinkAbout.AutoSize = true;
+            this.lblLinkAbout.Location = new System.Drawing.Point(337, 157);
+            this.lblLinkAbout.Name = "lblLinkAbout";
+            this.lblLinkAbout.Size = new System.Drawing.Size(35, 13);
+            this.lblLinkAbout.TabIndex = 6;
+            this.lblLinkAbout.TabStop = true;
+            this.lblLinkAbout.Text = "About";
+            this.lblLinkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // NSRForm
+            // 
+            this.AcceptButton = this.btnDownload;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 176);
+            this.Controls.Add(this.lblLinkAbout);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtBoxFileName);
+            this.Controls.Add(this.txtBoxURL);
+            this.Controls.Add(this.lblPromptFileName);
+            this.Controls.Add(this.lblPromptURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NSRForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nico Sound Reborn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPromptURL;
+        private System.Windows.Forms.Label lblPromptFileName;
+        private System.Windows.Forms.TextBox txtBoxURL;
+        private System.Windows.Forms.TextBox txtBoxFileName;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.LinkLabel lblLinkAbout;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * 
+ * References:
+ * http://stackoverflow.com/questions/1469764/run-command-prompt-commands
+ * http://stackoverflow.com/questions/206323/how-to-execute-command-line-in-c-get-std-out-results
+ * 
+ * 
+ * TO DO:
+ * - checkbox to keep mp4 file
+ * - check valid url
+ * - hide cmd window, redirect output to GUI
+ */
+
+namespace NicoSoundReborn
+{
+    public partial class NSRForm : Form
+    {
+        public NSRForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            // check if url is provided
+            if (txtBoxURL.Text.Equals(""))
+                lblProgress.Text = "Enter an appropriate NND URL";
+            else
+                // TO DO: what if the URL is invalid?
+                DownloadCommands();
+
+        }
+
+        /*
+         * Run through all the commands necessary to download a NND video 
+         */
+        private void DownloadCommands()
+        {
+            // start a new process
+            Process p = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            // set process params to download
+            startInfo.Arguments = "/C youtube-dl " + txtBoxURL.Text;
+            startInfo.FileName = "cmd.exe";
+            //startInfo.UseShellExecute = false;
+            //startInfo.RedirectStandardOutput = true;
+            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.StartInfo = startInfo;
+            p.Start();
+            lblProgress.Text = "Downloading...";
+
+            /*while (p.HasExited == false)
+            {
+                lblProgress.Text = p.StandardOutput.ReadToEnd();
+                lblProgress.Refresh();
+            }*/
+
+            p.WaitForExit();
+
+            lblProgress.Text = "Converting...";
+
+            // TO DO: extract mp4 name
+
+            // rename mp4
+            startInfo.Arguments = "/C ren *.mp4 tmp.mp4";
+            p.Start();
+            p.WaitForExit();
+
+            // check if user inputted file name, otherwise use default
+            // also check for ".mp3" and add automatically if not present
+            string fileName;
+            if (txtBoxFileName.Text.Equals(""))
+                fileName = "song.mp3";
+            else
+            {
+                if (txtBoxFileName.Text.EndsWith(".mp3", true, null))
+                    fileName = "\"" + txtBoxFileName.Text + "\"";
+                else
+                    fileName = "\"" + txtBoxFileName.Text + ".mp3" +"\"";
+            }
+
+            // convert mp4
+            startInfo.Arguments = "/C ffmpeg -i tmp.mp4 -f mp3 -ab 320000 -vn " + fileName;
+            p.Start();
+            p.WaitForExit();
+
+            // cleanup
+            startInfo.Arguments = "/C del tmp.mp4";
+            p.Start();
+            p.WaitForExit();
+
+            lblProgress.Text = "Complete!";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string about = "Version 1.1\n" +
+                            "Author: Peter Buk\n\n" +
+                            "This program makes use of these great utilities:\n" +
+                            "https://www.ffmpeg.org/\n" +
+                            "http://rg3.github.io/youtube-dl/index.html\n";
+
+            MessageBox.Show(about, "About");
+        }
+
+        
+
+    }
+}
